2fc8d8c9cd81dd9ec9260f0c0922252f43a0a498
Jan Kulose <jan.kulose@bonial.com>
2025-01-09 06:48:45 +0100

    fixed circular reference

--- src/main/java/de/kulose/musicquizhost/service/ValidationService.java
+++ src/main/java/de/kulose/musicquizhost/service/ValidationService.java
@@ -1,19 +1,13 @@
 package de.kulose.musicquizhost.service;
 
-import de.kulose.musicquizhost.models.Room;
-import de.kulose.musicquizhost.models.Round;
-import de.kulose.musicquizhost.models.SongData;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class ValidationService {
-    @Autowired
-    RoomService roomService;
 
     public void updatePoints(String roomId, String playerId, int roundIndex) {
-        Round round = roomService.getRound(roomId, roundIndex);
+        log.info("I need to calculate points");
     }
 }
